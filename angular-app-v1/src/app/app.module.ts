4a3aa175b6b7a6ef2a929774266abc5589def542
deepakbhalla <deepak.dbhalla@gmail.com>
2020-08-27 02:33:33 +0530

    added alerts, warningAlert, successAlert components

--- angular-app-v1/src/app/app.module.ts
+++ angular-app-v1/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warningAlert/warningAlert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { AlertsComponent } from './alerts/alerts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    AlertsComponent
   ],
   imports: [
     BrowserModule
